462d3e76625487b6b420a1c3c283a2267482ee64
Lucas Rocha <ld.rocha@hotmail.com>
2022-03-04 16:31:36 -0800

    add visual confirmation when emoji is copied '

--- src/components/CopyButton.tsx
+++ src/components/CopyButton.tsx
@@ -1,5 +1,6 @@
 import { CopyIcon } from "@chakra-ui/icons";
-import { Button, Flex, Skeleton } from "@chakra-ui/react";
+import { Button, Skeleton } from "@chakra-ui/react";
+import { useState } from "react";
 
 interface CopyButtonProps {
   emojiString: string;
@@ -7,18 +8,44 @@ interface CopyButtonProps {
 }
 
 export function CopyButton(props: CopyButtonProps) {
-  const { emojiString, isLoading } = props;
+  const { emojiString, isLoading} = props;
+
+  const [isActive, setIsActive] = useState(false);
+  console.log(isActive)
+
+
+  function copyAndDisplayCopyConfirmation() {
+    setIsActive(true);
+    navigator.clipboard.writeText(emojiString)
+    setTimeout(() => {
+      setIsActive(false);
+    }, 2000)
+  }
   return (
     <Skeleton startColor="gray.600" endColor="gray.700" isLoaded={!isLoading}>
-      <Button
-        m={2}
-        size="sm"
-        colorScheme="teal"
-        leftIcon={<CopyIcon />}
-        onClick={() => navigator.clipboard.writeText(emojiString)}
-      >
-        Copy
-      </Button>
+      {isActive === false ? (
+        <Button
+          m={2}
+          size="sm"
+          colorScheme="teal"
+          leftIcon={<CopyIcon />}
+          onClick={() => copyAndDisplayCopyConfirmation()}
+          isActive={isActive}
+        >
+          Copy
+        </Button>
+      ) : (
+        <Button
+          m={2}
+          size="sm"
+          colorScheme="teal"
+          leftIcon={<CopyIcon />}
+          onClick={() => copyAndDisplayCopyConfirmation()}
+          isActive={isActive}
+        >
+          Copied!
+        </Button>
+      )}
     </Skeleton>
   );
 }
